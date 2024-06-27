862d0ef3f42a735396ab3bd24ba54ac035f55f2b
Yan_Gabriel <yangabriel2012@gmail.com>
2024-06-27 00:16:46 -0400

    adicionando submit de login

--- src/app/login/components/LoginForm/index.tsx
+++ src/app/login/components/LoginForm/index.tsx
@@ -2,8 +2,10 @@
 import { useState } from "react"
 import { SignForm } from "../SignForm"
 import { SignUpForm } from "../SignUpForm"
-import { useForm } from "react-hook-form"
+import { SubmitHandler, useForm } from "react-hook-form"
 import { Form } from '@/components/ui/form'
+import { LoginFormData } from "@/interfaces/loginForm.interface"
+import { userLogin, userRegister } from "@/services/login.service"
 
 interface LoginForm {
 }
@@ -11,11 +13,28 @@ interface LoginForm {
 export const LoginForm = ({ }: LoginForm) => {
     const [showSignUpForm, setShowSignUpform] = useState(false)
     const form = useForm()
+
     const openSignUpForm = () => { setShowSignUpform(true), form.reset() }
+
     const closeSignUpForm = () => { setShowSignUpform(false), form.reset() }
 
-    const onSubmit = () => { }
-    console.log(form.watch());
+    const onSubmit: SubmitHandler<LoginFormData> = async (formData) => {
+        try {
+            const { user_confirm_password, user_email, user_name, user_password } = formData
+
+            showSignUpForm ? await userRegister({
+                confirmPassword: user_confirm_password ?? '',
+                email: user_email ?? '',
+                name: user_name ?? '',
+                password: user_password ?? ''
+            }) : await userLogin({
+                email: user_email ?? '',
+                password: user_password ?? ''
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
         <Form {...form}>
