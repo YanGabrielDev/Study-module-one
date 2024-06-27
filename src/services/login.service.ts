3760a8027eacddf1e132315a6d778d559aee183b
Yan_Gabriel <yangabriel2012@gmail.com>
2024-06-27 00:17:27 -0400

    criando services de login

---
+++ src/services/login.service.ts
@@ -0,0 +1,20 @@
+import { LoginData, RegisterData } from "@/interfaces/login.interface";
+import { api } from "@/lib/api"
+
+export const userLogin = async (data: LoginData) => {
+    try {
+        const response = await api.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/user/login`, data)
+        return response.data
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const userRegister = async (data: RegisterData) => {
+    try {
+        const response = await api.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/user`, data)
+        return response.data
+    } catch (error) {
+        console.error(error);
+    }
+}
