5a1d6cc1390555f368f79db423af4ac903d43934
Yan_Gabriel <yangabriel2012@gmail.com>
2024-07-02 21:24:02 -0400

    adicionando link para o outro modulo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Separator } from "@/components/ui/separator";
 import { getTasks } from "@/services/tasks.service";
+import Link from "next/link";
 import { useEffect } from "react";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       <div className="col-span-2 h-screen bg-darkGray p-4">
         <span className="text-white text-2xl">Task Manager</span>
         <Separator className="my-4" />
+        <Link className="text-white" href="http://localhost:3001" target="_blank">Module 2</Link>
       </div>
     </main>
   );
