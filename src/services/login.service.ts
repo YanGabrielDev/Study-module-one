9d1b96cd7d7c1775c1598b80e7137d8f9828a13b
Yan_Gabriel <yangabriel2012@gmail.com>
2024-07-09 23:59:22 -0400

    removendo parametro

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -3,9 +3,7 @@ import { api } from "@/lib/api"
 
 export const userLogin = async (data: LoginData) => {
     try {
-        const response = await api.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/user/login`, data, {
-            // withCredentials: true
-        })
+        const response = await api.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/user/login`, data)
         return response
     } catch (error) {
         console.error(error);
@@ -14,9 +12,7 @@ export const userLogin = async (data: LoginData) => {
 
 export const userRegister = async (data: RegisterData) => {
     try {
-        const response = await api.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/user`, data, {
-            // withCredentials: true 
-        })
+        const response = await api.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/user`, data)
         return response
     } catch (error) {
         console.error(error);
