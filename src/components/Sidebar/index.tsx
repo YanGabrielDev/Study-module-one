7295bf4ca99e961972562ce8e8b5429279cc3813
Yan_Gabriel <yangabriel2012@gmail.com>
2024-07-09 23:57:27 -0400

    adicionando itens na sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,11 +3,19 @@ import { Separator } from "../ui/separator"
 import { SidebarHeader } from "./SidebarHeader"
 import { SidebarCollapsedButton } from "./SidebarCollapsedButton"
 import { useState } from "react"
+import { USER_KEY } from "@/constants/localStorageKeys.constants"
+import { useLocalStorage } from "@/hooks/useLocalStorage"
+import { SidebarItem } from "./SidebarItem"
+import { MENU_MODULES } from "@/constants/modules.constants"
+import Link from "next/link"
+import { MODULE_ICONS } from "@/constants/moduleIcons.constants"
 
 export const Sidebar = () => {
     const [openSidebar, setOpenSidebar] = useState(false)
-
+    const { getLocalStorageItem } = useLocalStorage(USER_KEY)
+    const { modules } = getLocalStorageItem()
     const handleChangeOpenSidebar = () => setOpenSidebar(!openSidebar)
+    console.log(modules);
 
     return (
         <>
@@ -19,6 +27,16 @@ export const Sidebar = () => {
                     </div>
                     <Separator className="my-4 bg-white opacity-20" />
                 </div>
+                <ul>
+                    {modules?.map((module: "Study Timer") => (
+                        <SidebarItem>
+                            <Link
+                                className="no-underline text-white whitespace-nowrap flex gap-6"
+                                href={MENU_MODULES[module] ?? ''}
+                                target="_blank">{MODULE_ICONS[module]} {module}</Link>
+                        </SidebarItem>
+                    ))}
+                </ul>
                 <button><span className="text-white">Opa</span></button>
             </div>
         </>
